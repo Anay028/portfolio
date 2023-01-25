7a5566e7847d022f57c573db86d693f855312e2c
Anay <anayparswani2008@gmail.com>
2023-01-25 23:42:41 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector(elementName).innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, 150);
+  }
+}
+var i = 0;
+var txt = 'Hi, I m Anay,'; /* The text */
+elementName = '.typeWriter1'
+$(elementName).text('')
+$(elementName).css("visibility", 'visible')
+typeWriter()
+setTimeout(()=> {
+elementName = '.typeWriter2'
+i = 0   
+txt = "A Programmer"
+$(elementName).text('')
+$(elementName).css("visibility", 'visible')
+setTimeout(() => $('.viewSkills').css('visibility', 'visible'), 50 * txt.length)
+typeWriter()
+} , 150 * txt.length)
+
+function openEmail(){
+  window.location = "mailto:smog_snag.0s@icloud.com"
+}
